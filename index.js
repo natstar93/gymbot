77e53f3a3455273f34d1bdc0d613f57cce1259f6
Natalie Akam <natalie.akam@gmail.com>
2018-11-04 23:03:02 +0000

    Log user in

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const puppeteer = require('puppeteer');
+const { username, password} = require('./credentials.json');
 
 (async() => {
   const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://gymbox.legendonlineservices.co.uk/enterprise/account/login');
   console.log(await browser.version());
+  await page.waitFor('input[name="login.Email"]');
+
+  await page.$eval('input[name="login.Email"]', (el, username) => el.value = username, username);
+  await page.$eval('input[name="login.Password"]', (el, password) => el.value = password, password);
+
+  await page.click('input[type="submit"]');
+
+  await page.waitForResponse('https://gymbox.legendonlineservices.co.uk/enterprise/account/home');
+
   await browser.close();
 })();

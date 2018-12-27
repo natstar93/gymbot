038b1eec80bdeb7f4f49501d0b97e34086ba59ca
Natalie Akam <natalie.akam@gmail.com>
2018-12-27 17:30:04 +0000

    Add rest of class booking flow

---
+++ constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  login: 'https://gymbox.legendonlineservices.co.uk/enterprise/account/login',
+  home: 'https://gymbox.legendonlineservices.co.uk/enterprise/account/home',
+  timetable: 'https://gymbox.legendonlineservices.co.uk/enterprise/BookingsCentre/MemberTimetable',
+  paymentConfirmed: 'https://gymbox.legendonlineservices.co.uk/enterprise/basket/paymentconfirmed'
+}
--- index.js
+++ index.js
@@ -1,19 +1,71 @@
 const puppeteer = require('puppeteer');
-const { username, password} = require('./credentials.json');
+const { username, password } = require('./credentials.json');
+const pageUrls = require('./constants.js')
+
+const classId = process.argv[2];
+
+if(!classId || classId.search(/(slot)\d\w+/i) !== 0) {
+  console.log('Please enter a class Id eg slot000000');
+  return;
+}
 
 (async() => {
+  console.log(`Attempting to book ${classId}`);
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://gymbox.legendonlineservices.co.uk/enterprise/account/login');
-  console.log(await browser.version());
+  await page.goto(pageUrls.login);
+
   await page.waitFor('input[name="login.Email"]');
+
+  console.log('Entering login details');
 
   await page.$eval('input[name="login.Email"]', (el, username) => el.value = username, username);
   await page.$eval('input[name="login.Password"]', (el, password) => el.value = password, password);
 
+  console.log('Submitting login details');
+
   await page.click('input[type="submit"]');
 
-  await page.waitForResponse('https://gymbox.legendonlineservices.co.uk/enterprise/account/home');
+  try {
+    await page.waitForResponse(pageUrls.home);
+  } catch(err) {
+    console.log('Homepage did not load. Manually navigating to homepage');
+    await page.goto(pageUrls.home);
+  }
+
+  console.log('Logged in. Navigating to timetable');
+
+  try {
+    await page.goto(pageUrls.timetable);
+    await page.waitFor('table#MemberTimetable');
+  } catch(err) {
+    console.log('Could not load timetable');
+    return;
+  }
+
+  console.log(`Adding ${classId} to basket`);
+
+  try {
+    await page.click(`a#${classId}`);
+  } catch(err) {
+    console.log(`Could not find class ${classId}`, err);
+    return;
+  }
+
+  try {
+    console.log('Waiting for basket');
+    await page.waitFor('div.basketButtonWrapper', { timeout: 10000 });
+  } catch(err) {
+    console.log('Basket page not loaded', err);
+    return;
+  }
+
+  await page.click('a#btnPayNow');
+
+  await page.goto(pageUrls.paymentConfirmed);
+
+  console.log('Class booked!');
 
   await browser.close();
 })();
